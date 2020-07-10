cef5df8a31927b6044b619e905ab30fbf3ea633c
zaynhere <zain.geneve@gmail.com>
2020-07-10 14:13:00 +0500

    Project2-Covid19-Tracker

--- src/components/covid-logo.js
+++ src/components/covid-logo.js
@@ -4,7 +4,7 @@ import logo from '../images/covid-logo.jpg'
 const CovidLogo = () => {
     return (
         <div className="logo">
-            <img  src={logo} alt="image" width="350" height="100" />
+            <img  src={logo} alt="logo" width="350" height="100" />
         </div>
     );
 }
